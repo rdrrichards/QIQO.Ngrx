3e3c0d1e0b71f9afdcb362bb4783a20857e73cd0
Richard Richards <rdrrichards@hotmail.com>
2019-06-15 05:25:06 -0500

    batch

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,4 +1,5 @@
 CALL ng update @angular/cli
-CALL git commit -m "cli update to latest"
+CALL git commit -am "cli update to latest"
 CALL ng update @angular/core
+CALL git commit -am "ng update to latest"
 CALL npm audit fix
